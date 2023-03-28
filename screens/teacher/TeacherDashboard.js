b87d2ff824774c07f4064b5d39305199f9382a1f
Thibault Feraux <thibault.feraux@hotmail.com>
2023-03-28 19:31:13 +0200

    styling dashboard

--- screens/teacher/TeacherDashboard.js
+++ screens/teacher/TeacherDashboard.js
@@ -2,10 +2,11 @@ import { View, Text, TouchableOpacity, Alert } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { supabase } from '../../lib/supabase';
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
-import { ArrowRightOnRectangleIcon, PlusIcon } from 'react-native-heroicons/outline';
+import { ArrowRightOnRectangleIcon, ArrowUpRightIcon, PlusIcon } from 'react-native-heroicons/outline';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const TeacherDashboard = ({ route }) => {
     const { session } = route.params;
@@ -74,6 +75,8 @@ const TeacherDashboard = ({ route }) => {
                 .from('lesson')
                 .select('id, date, startTime, endTime, course(name), classroomtag(name)')
                 .eq('course.teacherId', session?.user.id)
+                .order('date', { ascending: true })
+                .order('startTime', { ascending: true })
 
             if (error && status !== 406) {
                 throw error
@@ -97,6 +100,30 @@ const TeacherDashboard = ({ route }) => {
         }
     }
 
+    // check if date is equal to today
+    const isToday = (date) => {
+        const today = new Date()
+        const someDate = new Date(date)
+        return someDate.getDate() == today.getDate() &&
+            someDate.getMonth() == today.getMonth() &&
+            someDate.getFullYear() == today.getFullYear()
+    }
+
+    const formatDate = (date) => {
+        const someDate = new Date(date)
+        // weekday as short form in dutch
+        const weekday = someDate.getDay();
+        const weekdays = ["Zo", "Ma", "Di", "Wo", "Do", "Vr", "Za"];
+        const weekdayName = weekdays[weekday];
+        // day with leading zero
+        const day = String(someDate.getDate()).padStart(2, '0');
+        // month with leading zero
+        const month = String(someDate.getMonth() + 1).padStart(2, '0');
+
+        return `${weekdayName} ${day}/${month}`
+    }
+
+
     // if fonts are not loaded, return null
     if (!fontsLoaded) {
         return null;
@@ -108,7 +135,7 @@ const TeacherDashboard = ({ route }) => {
     }
 
     return (
-        <SafeAreaView className="flex-1 justify-start bg-white px-7 pt-14">
+        <SafeAreaView className="flex-1 justify-start bg-slate-50 px-7 pt-14">
             {loading ? (
                 <Text>Loading...</Text>
             ) : (
@@ -124,6 +151,32 @@ const TeacherDashboard = ({ route }) => {
                             <PlusIcon color="white" size={24} />
                             <Text style={{ fontFamily: 'Poppins_500Medium' }} className="text-white mt-[2px]">Nieuwe les</Text>
                         </TouchableOpacity>
+                    </View>
+                    <View className="mt-4 space-y-4">
+                        {lessons.map((lesson, index) => (
+                            <View key={index} className={`border-b-gray-300 pb-3 ${index === lessons.length - 1 ? 'border-none' : 'border-b-[1px]'}`}>
+                                <Text style={{ fontFamily: 'Poppins_500Medium' }} className="text-base mb-2 text-slate-400">{ isToday(lesson.date) ? 'Vandaag' : formatDate(lesson.date)}</Text>
+                                {lesson.items.map((item, index) => (
+                                    <View key={index} className="flex-row justify-between bg-white shadow-lg shadow-black/40 mb-2 p-3 rounded-2xl">
+                                        <View className="justify-between">
+                                            <Text style={{ fontFamily: 'Poppins_500Medium' }} className="text-base">{item.course.name}</Text>
+                                            <Text style={{ fontFamily: 'Poppins_400Regular' }} className="text-sm text-gray-500">{item.startTime} - {item.endTime}</Text>
+                                        </View>
+                                        <View className="justify-between items-end space-y-2">
+                                            <ArrowUpRightIcon color="#c4b5fd" size={24} />
+                                            <LinearGradient
+                                                colors={['#E5E6EA', '#F8FAFC', '#F8FAFC']}
+                                                start={{ x: 1, y: 0 }}
+                                                end={{ x: 1, y: 1 }}
+                                                className="px-2 py-[1px] rounded-full shadow-inner shadow-black/40 bg-slate-50"
+                                            >
+                                                <Text style={{ fontFamily: 'Poppins_500Medium' }} className="text-sm uppercase">{item.classroomtag.name}</Text>
+                                            </LinearGradient>
+                                        </View>
+                                    </View>
+                                ))}
+                            </View>
+                        ))}
                     </View>
                 </>
             )}
